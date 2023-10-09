e78087c57c82aa925cd5c5949d8ecb4ed042754a
donal <donal@github.com>
2023-10-09 14:54:07 +1100

    add a border around the entire window
    Which is the default screen.

--- ball/ball.c
+++ ball/ball.c
@@ -8,12 +8,12 @@
 int main() {
 
 	WINDOW *mainwin;
-	int ch, ch2;
+	int ch;
 
-	int x = 0, y = 0;
+	int x = 1, y = 1;
 	int w_width = 0, w_height = 0;
 	int next_x = 0;
-	int next_y = 0;
+	// int next_y = 0;
 	int direction_x = 1;
 
 	if ( (mainwin = initscr()) == NULL ) {
@@ -24,6 +24,8 @@ int main() {
 	curs_set(FALSE);
 	keypad(mainwin, TRUE);
 	nodelay(mainwin, TRUE);
+	box(mainwin, 0, 0);
+	refresh();
 
 	int paused = 0;
 
@@ -39,7 +41,7 @@ int main() {
 		if (paused == 1) {
 			switch ( ch ) {
 				case 'z':
-					x = 0;
+					x = 1;
 					break;
 				case 'd':
 					direction_x *= -1;
@@ -50,7 +52,7 @@ int main() {
 					}
 					break;
 				case KEY_LEFT:
-					if (x - 1 > 0) {
+					if (x - 1 > 1) {
 						x -= 1;
 					}
 					break;
@@ -73,6 +75,7 @@ int main() {
 		getmaxyx(mainwin, w_height, w_width);
 		clear();
 		mvprintw(y, x, "o");
+		box(mainwin, 0, 0);
 		refresh();
 		usleep(DELAY);
 
@@ -84,7 +87,7 @@ int main() {
 
 		next_x = x + direction_x;
 
-		if (next_x >= (w_width / 2) || next_x < 0) {
+		if (next_x >= (w_width / 2) || next_x < 1) {
 			direction_x *= -1;
 		} else {
 			x += direction_x;
